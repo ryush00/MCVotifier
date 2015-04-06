e1b5347976adfb0721af79e1f3a10f08684ea120
SeongHoon Ryu <ryush00@gmail.com>
2015-04-06 23:14:43 +0900

    Update votifier.rb

--- lib/votifier.rb
+++ lib/votifier.rb
@@ -3,3 +3,6 @@ require File.expand_path('../votifier/server', __FILE__)
 require File.expand_path('../votifier/client', __FILE__)
 require File.expand_path('../votifier/key', __FILE__)
 require File.expand_path('../votifier/minecraft_server', __FILE__)
+
+module Votifier
+end
